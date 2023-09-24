6747a4549798969baa625a252173dc2b26eed844
vivaan3757 <pralavsh07.ps@gmail.com>
2023-09-24 12:11:54 +0530

    First Commit

---
+++ src/main/java/dev/pralav/productservicecategory/ProductservicecategoryApplication.java
@@ -0,0 +1,13 @@
+package dev.pralav.productservicecategory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductservicecategoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductservicecategoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/pralav/productservicecategory/ProductservicecategoryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pralav.productservicecategory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductservicecategoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
